86b2e2c546f58c8a8d1ce0841509a24f243a483e
Michael Andric <andric.michael@gmail.com>
2015-09-21 16:18:14 +0200

    reorganized how I make gm mask

--- gm_masker.py
+++ gm_masker.py
@@ -7,7 +7,7 @@ Created on Mon Sep 21 12:50:43 2015
 
 import os
 import time
-from shelx import split
+from shlex import split
 from subprocess import call, STDOUT
 
 class Masker(object):
@@ -18,31 +18,25 @@ class Masker(object):
         self.subjid = ss
         self.stdoutdir = stdout_dir
 
-    def get_gm(self, fast_seg, outpref):
+    def get_gm(self, pve, outpref):
         print '3dcalc to get gm segmentation'
         f = open('%s/stdout_from_gm_seg.txt' % self.stdoutdir, 'w')
-        calcargs = split("3dcalc -a %s -expr 'equals(a, 2) -prefix %s" %
-        (fast_seg, outpref))
+#        calcargs = split("3dcalc -a %s -expr 'equals(a, 2) -prefix %s" %
+#        (fast_seg, outpref))
+        calcargs = split("3dcalc -a %s -expr 'step(a-.3)' -prefix %s" %
+        (pve, outpref))
         call(calcargs, stdout=f, stderr=STDOUT)
-        f.close()
-
-    def fractionize(self, input, outpref):
-        print 'Doing fractionize for %s' % outpref
-        f = open('%s/stdout_from_fractionize.txt' % self.stdoutdir, 'w')
-        frcargs = split('3dfractionize -template %s -input %s \
-                        -prefix %s -clip 0.3' % (self.ref, input, outpref))
-        call(frcargs, stdout=f, stderr=STDOUT)
         f.close()
 
     def mask_calc(self, subcort_frc, gm_frc, outpref):
         print 'Doing mask_calc'
         f = open('%s/stdout_from_mask_calc.txt' % self.stdoutdir, 'w')
-        mask_args = split("3dcalc -a %s -b %s -expr 'a+b' -prefix %s" %
+        mask_args = split("3dcalc -a %s -b %s -expr 'step(a+b)' -prefix %s" %
         (subcort_frc, gm_frc, outpref))
         call(mask_args, stdout=f, stderr=STDOUT)
         f.close()
 
-    def reorient(self, input, outname, premat, interp):
+    def applywarp_reorient(self, input, outname, premat, interp):
         print 'Reorient to epi space'
         f = open('%s/stdout_from_reorient.txt' % self.stdoutdir, 'w')
         cmdargs = split('applywarp -i %s -r %s -o %s \
@@ -56,29 +50,25 @@ if __name__ == '__main__':
 #    subj_list = range(1, 20)
     subj_list = [19]
     for ss in subj_list:
-        for i in range(1, 5):
-            anat_dir = os.path.join(os.environ['avp'], 'nii',
-                                    '%s_CNR.anat' % ss)
-            ref_brain = os.path.join(anat_dir, '%s_%s.vol8.nii.gz' % (ss, i))
-            msk = Masker(ref_brain, ss, anat_dir)
+        anat_dir = os.path.join(os.environ['avp'], 'nii',
+                                '%s_CNR.anat' % ss)
+        ref_brain = os.path.join(anat_dir, '%s_1.vol8.nii.gz' % (ss))
+        msk = Masker(ref_brain, ss, anat_dir)
 
-            fast_seg = os.path.join(anat_dir, 'T1_fast_seg.nii.gz')
-            gm_out_pref = os.path.join(anat_dir, '%s_%s.fast_seg_gm.nii.gz' %
-            (ss, i))
-            msk.get_gm(fast_seg, gm_out_pref)
+        pve_seg = os.path.join(anat_dir, 'T1_fast_pve_1.nii.gz')
+        gm_out_pref = os.path.join(anat_dir, 'pve_gm_seg.%s.nii.gz' % (ss))
+        msk.get_gm(pve_seg, gm_out_pref)
 
-            gm_frac_pref = os.path.join(anat_dir,
-                                        '%s_%s.fast_seg_gm_frac.nii.gz' %
-                                        (ss, i))
-            subcort = os.path.join(anat_dir, 'T1_subcort_seg.nii.gz')
-            subcort_frac_pref = os.path.join(anat_dir,
-                                             '%s_s.subcort_seg_frac.nii.gz')
-            msk.fractionize(fast_seg, gm_frac_pref)
-            msk.fractionize(subcort, subcort_frac_pref)
-            mask_pref = os.path.join(anat_dir,
-                                     '%s_%s.gm_mask_T1space.nii.gz' % (ss, i))
-            msk.mask_calc(subcort_frac_pref, gm_frac_pref, mask_pref)
-            reorient_pref = os.path.join(anat_dir,
-                                         '%s_%s.gm_mask_epi.nii.gz' % (ss, i))
-            pre_mat = os.path.join(anat_dir, 'mprage2.%s_%s.mat' % (ss, i))
-            msk.reorient(mask_pref, reorient_pref, pre_mat, 'spline')
+        pre_mat = os.path.join(anat_dir, 'mprage2.%s_1.mat' % (ss))
+        gm_reor_pref = os.path.join(anat_dir,
+                                    'pve_gm_seg_epispace.%s.nii.gz' % ss)
+        msk.applywarp_reorient(gm_out_pref, gm_reor_pref, pre_mat, 'nn')
+
+        subcort = os.path.join(anat_dir, 'T1_subcort_seg.nii.gz')
+        subcort_reor_pref = os.path.join(anat_dir,
+                                         'subcort_seg_epispace.%s.nii.gz' % ss)
+        msk.applywarp_reorient(subcort, subcort_reor_pref, pre_mat, 'nn')
+
+        mask_out_pref = os.path.join(anat_dir,
+                                     'gm_mask_epispace.%ss.nii.gz' % ss)
+        msk.mask_calc(subcort_reor_pref, gm_reor_pref, mask_out_pref)
