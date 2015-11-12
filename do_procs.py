ffb53fe467c61b390deb7a7e85ba2a951c0b6165
Michael Andric <andric.michael@gmail.com>
2015-11-12 13:13:09 +0100

    update do_procs to dump ss effects coef data

--- do_procs.py
+++ do_procs.py
@@ -12,13 +12,13 @@ if __name__ == '__main__':
     subj_list = range(1, 19)
 #    subj_list = [19]
 
-    group_dir = os.path.join(os.environ['avp'], 'nii',
-                             'group_effects')
     effects = ['Aentr', 'Ventr', 'Aentr_intxn']
     mask = os.path.join(os.environ['FSLDIR'], 'data/standard',
                         'MNI152_T1_2mm_brain_mask_dil1.nii.gz')
-    for ef in effects:
-        inpref = 'Clust_ttest_%s_mask+tlrc' % ef
-        clust_image = os.path.join(group_dir, inpref)
-        out_txt = os.path.join(group_dir, '%s.txt' % inpref)
-        pr.maskdump(group_dir, mask, clust_image, out_txt)
+    for ss in subj_list:
+        ef_dir = os.path.join(os.environ['avp'], 'nii', 'ss%s_effects' % ss)
+        for ef in effects:
+            inpref = '%s_ss%s_coef+tlrc' % (ef, ss)
+            clust_image = os.path.join(ef_dir, inpref)
+            out_txt = os.path.join(ef_dir, '%s.txt' % inpref)
+            pr.maskdump(ef_dir, mask, clust_image, out_txt)
