60419f315098bfe008a17b3ba98a001d3ac04412
Michael Andric <andric.michael@gmail.com>
2015-12-02 18:45:50 +0100

    init concat

---
+++ concat.py
@@ -0,0 +1,28 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Dec  2 18:38:22 2015
+
+@author: andric
+"""
+
+import os
+from subprocess import call
+from shelx import split
+
+def concat(filelist, outname):
+    files =  ' '.join(filelist)
+    cmd = split('cat %s > %s' % (files, outname))
+    call(cmd)
+
+if __name__ == '__main__':
+    subj_list = range(1, 20)
+    for ss in subj_list:
+        workdir = os.path.join(os.environ['avp'], 'nii', '%s_CNR.anat' % ss)
+        for n in ['wm', 'vent']:
+            inputs = []
+            for r in range(1, 5):
+                f = 'wm_v8.%s_%s.Powered.cleanEPI.uncensored.txt' % (ss, r)
+                inputs.append(os.path.join(workdir, f))
+            outn = 'wm_v8.%s_all.Powered.cleanEPI.uncensored.txt' % ss
+            outf = os.path.join(workdir, outn)
+            concat(inputs, outf)
