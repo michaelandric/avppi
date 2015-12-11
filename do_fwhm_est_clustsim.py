cddfa517160b5ccec43efb58dda33c6ab5995731
Michael Andric <andric.michael@gmail.com>
2015-12-11 01:37:36 +0100

    re-doing fwhm_est_clustsim

--- do_fwhm_est_clustsim.py
+++ do_fwhm_est_clustsim.py
@@ -6,6 +6,7 @@ Created on Tue Nov 10 16:17:02 2015
 """
 
 import os
+import setLog
 import numpy as np
 import procs as pr
 from shlex import split
@@ -21,24 +22,33 @@ def get_fwhm(input):
     return map(float, p[0].split())
 
 
-subj_list = range(1, 19)
+subj_list = range(1, 20)
+subj_list.remove(3)
+subj_list.remove(11)
 xyz_mat = np.zeros(len(subj_list)*3).reshape(len(subj_list), 3)
 decon_dir = os.path.join(os.environ['avp'],
                          'nii', 'deconvolve_outs_concat')
 suff = 'Powered.cleanEPI_errts_REML_mean_fnirted_MNI2mm'
+lg = setLog._log('%s/fwhm_est_clustsim' % decon_dir)
+lg.info('Doing get_fwhm')
 for i, ss in enumerate(subj_list):
+    lg.info('subj %s' % ss)
     fpref = 'decon_out.mion.%s_concat.%s' % (ss, suff)
     input_pref = os.path.join(decon_dir, fpref)
     x, y, z = get_fwhm('%s.nii.gz' % input_pref)
     xyz_mat[i, :] = [x, y, z]
+lg.info('get_fwhm done.')
 
 avg_fwhm = np.mean(xyz_mat, axis=0)
-print 'FWHM is:'
-print avg_fwhm
-out_pref = 'decon_out.mion.group_.%s_fwhm_est_out' % suff
+lg.info('FWHM is: %s' % avg_fwhm)
+out_pref = 'fwhm_est_decon_out.mion.group.%s_out' % suff
 out_name = os.path.join(decon_dir, out_pref)
 np.savetxt(out_name, avg_fwhm.reshape(1, 3), fmt='%.4f %.4f %.4f')
+lg.info('saved FWHM to %s' % out_name)
 
 mask = os.path.join(os.environ['FSLDIR'], 'data/standard',
                     'MNI152_T1_2mm_brain_mask_dil1.nii.gz')
+
+lg.info('Doing clustsim')
 pr.clustsim(avg_fwhm, decon_dir, mask)
+lg.info('clustsim done.')
