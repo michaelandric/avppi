0b18a7c0b278646e2dc88f95b9ce99683c61a0cd
Michael Andric <andric.michael@gmail.com>
2015-12-15 13:57:04 +0100

    update do_procs mask dump coef from effects

--- do_procs.py
+++ do_procs.py
@@ -9,11 +9,9 @@ import os
 import procs as pr
 
 if __name__ == '__main__':
-    subj_list = range(1, 19)
-    subj_list.remove(11)
-#    subj_list = [19]
+    subj_list = [s for s in range(1, 20)]
 
-#    effects = ['Aentr', 'Ventr', 'Aentr_intxn']
+    effects = ['Aentr', 'Ventr', 'Aentr_intxn']
     conds = ['ALowVLow', 'ALowVHigh', 'AHighVLow', 'AHighVHigh']
     sb = [4, 7, 10, 13]
     d = dict(zip(conds, sb))
@@ -21,6 +19,7 @@ if __name__ == '__main__':
                              'deconvolve_outs_concat_dec')
     mask = os.path.join(os.environ['FSLDIR'], 'data/standard',
                         'MNI152_T1_2mm_brain_mask_dil1.nii.gz')
+    """
     suff = 'Powered.cleanEPI_REML_fnirted_MNI2mm'
     for ss in subj_list:
         ef_dir = os.path.join(os.environ['avp'], 'nii', 'ss%s_effects_dec' % ss)
@@ -30,3 +29,11 @@ if __name__ == '__main__':
             out_txt = "%s/%s_coef_%s_concat.%s.txt" % \
                 (decon_dir, cn, ss, suff)
             pr.maskdump(ef_dir, mask, in_image, out_txt)
+    """
+    for ss in subj_list:
+        ef_dir = os.path.join(os.environ['avp'], 'nii', 'ss%s_effects_dec' % ss)
+        for ef in effects:
+            inpref = '%s_ss%s_coef+tlrc' % (ef, ss)
+            clust_image = os.path.join(ef_dir, inpref)
+            out_txt = os.path.join(ef_dir, '%s.txt' % inpref)
+            pr.maskdump(ef_dir, mask, clust_image, out_txt)
