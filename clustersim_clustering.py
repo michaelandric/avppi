f8acc2401758987275b33153247d4e1f5dfdaf34
Michael Andric <andric.michael@gmail.com>
2016-01-12 13:31:03 +0100

    adjusted clustersim_clustering params

--- clustersim_clustering.py
+++ clustersim_clustering.py
@@ -47,7 +47,7 @@ def cluster(outdir, thr, clst_size, infile, outname):
     lg.info('Doing cluster ---- ')
     cmd = split(('3dclust -prefix %s -1Dformat -nosum -1dindex 1 -1tindex 1 \
                  -2thresh -%s %s \
-                 -dxyz=2 1.01 %s %s -savemask %s_mask') %
+                 -dxyz=1 1.44 %s %s -savemask %s_mask') %
                  (outname, thr, thr, clst_size, infile, outname))
     p = subprocess.run(cmd, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
     lg.info(p.stdout.decode("utf-8", "strict"))
