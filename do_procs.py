2a8a89c062ba9546a8764eb8befe020d3674964e
Michael Andric <andric.michael@gmail.com>
2016-02-22 16:42:09 +0100

    update do_procs.py to account for diff blk lengths

--- do_procs.py
+++ do_procs.py
@@ -11,15 +11,19 @@ import procs as pr
 if __name__ == '__main__':
     subj_list = [s for s in range(1, 20)]
 
-    group_dir = os.path.join(os.environ['avp'], 'nii',
-                             'group_effects_dec')
-
-    """Doing p < .005. There are no Ventr clusters"""
-    effects = ['Aentr', 'Aentr_intxn']
+    effects = ['Aentr', 'Ventr', 'Aentr_intxn']
     mask = os.path.join(os.environ['FSLDIR'], 'data/standard',
                         'MNI152_T1_2mm_brain_mask_dil1.nii.gz')
-    for ef in effects:
-        inpref = 'clust_%s_flt2_msk_mema_p.005_mask+tlrc' % ef
-        clust_image = os.path.join(group_dir, inpref)
-        out_txt = os.path.join(group_dir, '%s.txt' % inpref)
-        pr.maskdump(group_dir, mask, clust_image, out_txt)
+
+    for block in [20, 15, 10]:
+        outdir = os.path.join(os.environ['avp'], 'nii',
+                              'group_effects_%sblk' % block)
+        for ef in effects:
+            ef_pref = os.path.join(outdir,
+                                   'clust_%s_flt2_%sblk_msk_mema_mask+tlrc' %
+                                   (ef, block))
+            ef_file = '%s.BRIK.gz' % ef_pref
+            if os.path.exists('%s.BRIK.gz' % ef_pref):
+                clust_image = ef_file
+                out_txt = os.path.join(outdir, '%s.txt' % ef_pref)
+                pr.maskdump(outdir, mask, clust_image, out_txt)
